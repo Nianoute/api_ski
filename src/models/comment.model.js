51db5ab0afc9b9be46925ee3ed784edf221a7fb7
Nianoute <enzo.angot@gmail.com>
2023-02-09 21:50:43 +0100

    v1?

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -6,6 +6,21 @@ const commentSchema = new Schema({
     type: String,
     required: true,
   },
+  description: {
+    type: String,
+    required: true,
+  },
+  stars: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
   post: {
     type: Schema.Types.ObjectId,
     ref: "Post",
--- src/models/post.model.js
+++ src/models/post.model.js
@@ -6,12 +6,52 @@ const postSchema = new Schema({
     type: String,
     required: true,
   },
+  imageUrl: {
+    type: String,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+    enum: [140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190],
+  },
+  style: {
+    type: String,
+    required: true,
+    enum: ["Freeride", "Freestyle", "Piste", "Polyvalent"],
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
   bookings: [
     {
       type: Schema.Types.ObjectId,
       ref: "Booking",
     },
   ],
+  isAvailable: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
 });
 
 const Post = mongoose.model("Post", postSchema);
